94d7f225f06e81f71c94db46b0daa5cbc20b4dd7
tiarra11 <tmcin0329@gmail.com>
2019-05-08 19:57:14 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,29 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["her", "our", "mine", "him"];
+
+  let adj = ["elegant", "petite", "brave", "nice"];
+
+  let noun = ["ankle", "body", "bat", "cap"];
+
+  let string = "";
+
+  for (let i = 0; i < noun.length; i++) {
+    string = string + pronoun[i] + adj[i] + noun[i] + ".com </br>";
+  }
+
+  document.querySelector(".domain").innerHTML = string;
+  ("Hello I'm not hungry anymore because I ate that whole place.");
+
+  //   let part2 = pronoun[1] + adj[1] + noun[1] + ".com";
+  //   console.log(part2);
+
+  //   let part3 = pronoun[2] + adj[2] + noun[2] + ".com";
+  //   console.log(part3);
+
+  //   let part4 = pronoun[3] + adj[3] + noun[3] + ".com";
+  //   console.log(part4);
+
+  //   console.log("Hello Rigo from the console!!!!");
 };
